4a950d4eba4642524a7aa36353319f6fb2e18d1f
KurtV731 <kp.vogelsaenger@gmail.com>
2026-08-14 20:17:36 +0200

    SERKAL 0.0.4: Sprache an TMDB-Suche weiterreichen

--- 2 src/common/preload.js
+++ 2 src/common/preload.js
@@ -10,7 +10,7 @@ contextBridge.exposeInMainWorld("serkal", {
         status:()=>ipcRenderer.invoke("serkal:tmdb:status"),
         saveKey:(apiKey)=>ipcRenderer.invoke("serkal:tmdb:saveKey",apiKey),
         test:()=>ipcRenderer.invoke("serkal:tmdb:test"),
-        searchTv:(query)=>ipcRenderer.invoke("serkal:tmdb:searchTv",query)
+        searchTv:(query,lang)=>ipcRenderer.invoke("serkal:tmdb:searchTv",query,lang)
     },
     calendar:{ open:(settings)=>ipcRenderer.invoke("serkal:calendar:open",settings) }
 });
