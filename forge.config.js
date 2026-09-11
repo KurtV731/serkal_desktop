9636a592d580f74e3ddaf8d93ade9d6d262b9fac
KurtV731 <kp.vogelsaenger@gmail.com>
2026-09-11 23:06:17 +0200

    Squirrel-Setup für SerKal Desktop 1.0 benennen

--- forge.config.js
+++ forge.config.js
@@ -5,7 +5,7 @@ module.exports = {
   packagerConfig: {
     asar: true,
 
-    // SERKAL Desktop 0.9002:
+    // SERKAL Desktop 1.0:
     // Nur Laufzeit-Abhaengigkeiten ins Paket nehmen. Forge/Electron-
     // Entwicklungswerkzeuge gehoeren nicht in app.asar.
     prune: true,
@@ -36,7 +36,7 @@ module.exports = {
         name: 'SerKalDesktop',
         authors: 'Kurt Vogelsaenger',
         description: 'SERKAL Desktop – Serienkalender',
-        setupExe: 'SerKal_0.9002_Setup.exe',
+        setupExe: 'SerKal_1.0_Setup.exe',
         setupIcon: '4 assets/icon/serkal.ico',
 
         // Squirrel versucht sonst zusaetzlich, Update.exe mit demselben Icon
