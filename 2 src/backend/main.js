50a8b4eb091049db139f2f871d319c83c554cb12
KurtV731 <kp.vogelsaenger@gmail.com>
2026-09-11 23:06:24 +0200

    Backend-Kopf auf SerKal Desktop 1.0 setzen

--- 2 src/backend/main.js
+++ 2 src/backend/main.js
@@ -3,7 +3,7 @@
  SERKAL Desktop
 -------------------------------------------------------------------------------
  Datei      : main.js
- Version    : 0.9002
+ Version    : 1.0
  Aufgabe    : Startet Electron, verwaltet lokale Grundeinstellungen,
               oeffnet den Kalender, stellt die TMDB-/SERKAL-Suche bereit
               und portiert das SERKAL-2.5-Archiv auf lokale TXT-Dateien.
